79d979ab9678b3fb0fe093b6275fce243c70af36
James <JaimeHatchet@gmail.com>
2018-11-19 19:04:45 -0600

    20181119

--- Game/Card.cs
+++ Game/Card.cs
@@ -8,7 +8,7 @@ namespace GameHandler
     public class Card
     {
         public Suit Suit { get; private set;}
-        public Face Face { get; private set; }
+        public Face Face { get; private set;}
         
         public Card(Suit suit, Face face)
         {
